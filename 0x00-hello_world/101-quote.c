43622c08f1880a0f93d96856445c40dde0e1afe5
victormayowa <hardmanvicky01@gmail.com>
2023-02-16 13:43:45 -0800

    stdout and fputusing unix standard library

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main- entry
+ * prints a line to a standard error
+ *
+ * Return: return 1
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
