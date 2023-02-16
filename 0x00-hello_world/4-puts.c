83883eff5862e543696f2a3d1da6717084b03299
victormayowa <hardmanvicky01@gmail.com>
2023-02-16 12:36:25 -0800

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- an entry point to the function
+ * puts- prints statement
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
