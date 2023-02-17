583957de06e2061c47664c11f5eb7330ac2cdffa
victormayowa <hardmanvicky01@gmail.com>
2023-02-17 15:21:37 -0800

    combo 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main- prints all possible different combinations of three digits.
+* Return: 0
+*/
+int main(void)
+{
+	int o = '0';
+	int t = '0';
+	int h = '0';
+
+	for (h = '0'; h <= '9' ; h++)
+	{
+		for (t = '0' ; t < '9' ; t++)
+		{
+			for (o = '0' ; o <= '9' ; o++)
+			{
+				if (!((o == t) || (t == h) ||
+				(t > o) || (h > t)))
+				{
+					putchar(h);
+					putchar(t);
+					putchar(o);
+					if (!(o == '9' && h == '7' && t == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
