d9c5bf96bd1c4a362c59262aa1005d05e32d9e65
victormayowa <hardmanvicky01@gmail.com>
2023-02-22 11:44:04 -0800

    division trail

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	for (i = 0 ; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
