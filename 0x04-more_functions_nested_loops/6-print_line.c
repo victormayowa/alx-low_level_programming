3269b40ad3e4f5315d86d8ef093d2b478928647e
victormayowa <hardmanvicky01@gmail.com>
2023-02-23 15:30:30 -0800

    draw line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_line - draw a straight line
+* @n: first integer
+* Return: largest number
+*/
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
+
+
