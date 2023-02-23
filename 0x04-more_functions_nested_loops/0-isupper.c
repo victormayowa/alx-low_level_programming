4a93c59e4decc17a8d477e57553733515140531d
victormayowa <hardmanvicky01@gmail.com>
2023-02-23 14:23:53 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - returns the largest of 3 numbers
+* @x: first integer
+* Return: 1 for upper
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
