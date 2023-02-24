9cf6a9208be0540d7b8c7bd77da6788460a5ebac
victormayowa <hardmanvicky01@gmail.com>
2023-02-23 16:28:35 -0800

    complex

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_number - returns the largest of 3 numbers
+* @n: first integer
+* Return: largest number
+*/
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
+}
