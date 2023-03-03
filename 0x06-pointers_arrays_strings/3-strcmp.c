84de4b963c504bbab75b5645ae066bb6df1c9026
victormayowa <hardmanvicky01@gmail.com>
2023-03-02 18:50:56 -0800

    string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare string values
+ * @s1: Input value
+ * @s2: Input value
+ * Return: s1[i] - s2[i]
+ */
+
+
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (int)(*s1 - *s2);
+}
