0e00b0092d1ad6ffc660ca833e0a63e40f521bcf
victormayowa <hardmanvicky01@gmail.com>
2023-03-02 18:04:32 -0800

    concat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+void _puts(char *str);
+void print_number(int n);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
+
+#endif
