c36303adca9e39dd32dee4ad3bb3a2ea3c5244fb
victormayowa <hardmanvicky01@gmail.com>
2023-03-02 20:31:12 -0800

    addition

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 void _puts(char *str);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
