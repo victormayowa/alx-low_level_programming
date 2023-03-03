09bea05f29b6b173561c190d86b7eb958b31db35
victormayowa <hardmanvicky01@gmail.com>
2023-03-02 20:19:20 -0800

    leet

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void print_rev(char *s);
+char *leet(char *n);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
