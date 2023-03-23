a1b2c4cc792e10d3051d3e997f17066f811d9997
victormayowa <hardmanvicky01@gmail.com>
2023-03-23 15:07:55 -0700

    write numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ * @n: number of arguments
+ * @separator: character separator of numbers
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	va_start(list, n);
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d%s", va_arg(list, int), separator);
+	}
+	printf("%d\n", va_arg(list, int));
+}
