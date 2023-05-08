e5ac7ffe73496b0e3f6db61be14431f1d28209a1
Victor Mayowa <93038708+victormayowa@users.noreply.github.com>
2023-05-08 13:34:47 +0100

    update bit-manipulation
    update bit-manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
-  * binary_to_uint - converts a binary number to an unnsigned_int
+  * binary_to_uint - converts a binary number to an unsigned_int
   * @b: points to the address of a string of 0 and 1 chars
   * Return: converted number or 0 if there is one/more chars in the string b
   * that is not 0 or 1 or when b is NULL.
--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,8 +4,8 @@
   * get_endianness - check for endianess of a system which refers to the byte
   * order in which a multibyte value is stored in the memory. there are two
   * types of endianess: big-endian and little-endian
-  * in BE: the most significant byet is stored in the  lowest memory address
-  * wbbhile the least SB is stored at the  highest memory address. ordered left
+  * in BE: the most significant byte is stored in the  lowest memory address
+  * while the least SB is stored at the  highest memory address. ordered left
   * to right.
   * LE is vice-versa to BE.
   * Return: 0 if big endian and 1 if little endian
