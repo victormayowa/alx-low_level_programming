06b49301e940d67f3b10e19b839f939f3fdf8f64
victormayowa <hardmanvicky01@gmail.com>
2023-06-15 14:27:16 -0700

    list int

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/* Structure for doubly linked list node */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototypes */
+size_t dlistint_len(const dlistint_t *h);
+
+#endif /* LISTS_H */
+
